b488f9bcbb776d5ef0be888451eefe23070885a4
jsus <jsusmarcano.jm@gmail.com>
2022-09-04 23:10:00 -0400

    set up static server

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -14,11 +14,10 @@ func main() {
   if err != nil {
     log.Fatal("Error loading .env")
   }
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-    w.Header().Set("Content-Type", "text/plain")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("It's working!!"))
-  })
+
+  fs := http.FileServer(http.Dir(os.Getenv("STORAGE_DIR")))
+  http.Handle("/raw/", http.StripPrefix("/raw/", fs))
+
   port := os.Getenv("PORT")
   fmt.Println("Listen in port", port)
   http.ListenAndServe(port, nil)
